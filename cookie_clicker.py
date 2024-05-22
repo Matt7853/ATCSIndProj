3c6ac1155298a1a7090108acb2a0c0249e45eb48
Matthew Bristing <matthew@bristing.com>
2024-05-22 09:21:23 -0700

    Added remaining 9 buildings and their names, extended their length, attempted long number formatting

--- cookie_clicker.py
+++ cookie_clicker.py
@@ -1,13 +1,19 @@
 import sys
 import pygame as pg
 import time as t
+import math as m
 
 class Building(object):
-    def __init__(self, base_cost, base_cps):
+    def __init__(self, base_cost, base_cps, order, name):
+        self.name = name
         self.cost = base_cost
         self.cps = base_cps
         self.ammount = 0
         self.is_hidden = True
+        self.box = pg.Rect(760, order, 320, 40)
+
+    def get_box(self):
+        return self.box
 
     def get_cps(self):
         return self.cps
@@ -21,6 +27,9 @@ class Building(object):
     def get_number(self):
         return self.ammount
     
+    def get_name(self):
+        return self.name
+    
     def locked(self):
         return self.is_hidden == True
     
@@ -29,18 +38,67 @@ class Building(object):
         self.ammount += 1
     
 class Cursors(Building):
-    def __init__(self, cost = 15, cps = 1):
-        super().__init__(cost, cps)
+    def __init__(self, cost = 15, cps = 0.1, order = 0, name = "Cursor"):
+        super().__init__(cost, cps, order, name)
         self.is_hidden = False
 
 class Grandmama(Building):
-    def __init__(self, cost = 100, cps = 5):
-        super().__init__(cost, cps)
+    def __init__(self, cost = 100, cps = 1, order = 45, name = "Grandma"):
+        super().__init__(cost, cps, order, name)
+        
 
 class Farm(Building):
-    def __init__(self, cost = 1000, cps = 25):
-        super().__init__(cost, cps)
+    def __init__(self, cost = 1100, cps = 8, order = 90, name = "Farm"):
+        super().__init__(cost, cps, order, name)
+        
 
+class Mine(Building):
+    def __init__(self, cost = 12000, cps = 47, order = 135, name = "Mine"):
+        super().__init__(cost, cps, order, name)
+        
+
+class Factory(Building):
+    def __init__(self, cost = 130000, cps = 260, order = 180, name = "Factory"):
+        super().__init__(cost, cps, order, name)
+        
+
+class Bank(Building):
+    def __init__(self, cost = 1400000, cps = 1400, order = 225, name = "Bank"):
+        super().__init__(cost, cps, order, name)
+        
+
+class Temple(Building):
+    def __init__(self, cost = 20000000, cps = 7800, order = 270, name = "Temple"):
+        super().__init__(cost, cps, order, name)
+        
+
+class Wizerd(Building):
+    def __init__(self, cost = 330000000, cps = 44000, order = 315, name = "Tower"):
+        super().__init__(cost, cps, order, name)
+        
+
+class Enterprise(Building):
+    def __init__(self, cost = 5100000000, cps = 260000, order = 360, name = "Shipment"):
+        super().__init__(cost, cps, order, name)
+        
+
+class PEChem(Building):
+    def __init__(self, cost = 75000000000, cps = 1600000, order = 405, name = "Alchemist"):
+        super().__init__(cost, cps, order, name)
+
+class Sanchez(Building):
+    def __init__(self, cost = 1000000000000, cps = 10000000, order = 450, name = "Portal"):
+        super().__init__(cost, cps, order, name)
+
+"""
+def num_format(num, round_to=1):
+    magnitude = 0
+    while abs(num) >= 1000:
+        magnitude += 1
+        num = round(num / 1000.0, round_to)
+    return '{:.{}f}{}'.format(num, round_to, ['', '00', ' million', ' billion', ' trillion', 'T', 'P'][magnitude])
+"""
+# "{:.0f}".
 
 def main():
     pg.init()
@@ -53,16 +111,12 @@ def main():
     clock = pg.time.Clock()
     # A pygame.Rect to define the area.
     cookie = pg.Rect(20, 60, 250, 250)
-    buildbox = pg.Rect(880, 0, 200, 490)
+    buildbox = pg.Rect(760, 0, 320, 490)
 
     dev_cookup = pg.Rect(300, 285, 10, 10)
     dev_cpup =  pg.Rect(315, 285, 10, 10)
     dev_cookdown = pg.Rect(300, 300, 10, 10)
     dev_cpdown =  pg.Rect(315, 300, 10, 10)
-
-    curs_box = pg.Rect(880, 0, 200, 40)
-    grand_box = pg.Rect(880, 45, 200, 40)
-    farm_box = pg.Rect(880, 90, 200, 40)
 
     global COOKIES
     COOKIES = 0
@@ -74,12 +128,21 @@ def main():
     cursors = Cursors()
     grandmas = Grandmama()
     farms = Farm()
+    mines = Mine()
+    factories = Factory()
+    banks = Bank()
+    temples = Temple()
+    towers = Wizerd()
+    shipments = Enterprise()
+    alchemists = PEChem()
+    portals = Sanchez()
+    buildings = [cursors, grandmas, farms, mines, factories, banks, temples, towers, shipments, alchemists, portals]
     
     time_start = t.time()
 
     while not done:
-        if (t.time() - time_start) > 0.05:
-            COOKIES += (CPS * 0.05)
+        if (t.time() - time_start) > 0.03:
+            COOKIES += (CPS * 0.03)
             time_start = t.time()
         for event in pg.event.get():
             if event.type == pg.QUIT:
@@ -100,11 +163,11 @@ def main():
                         COOKIES += 1
                     elif buildbox.collidepoint(event.pos):
                         for building in buildings:
-                            if building[0].collidepoint(event.pos):
-                                if COOKIES >= building[1].get_price():
-                                    COOKIES -= building[1].get_price()
-                                    CPS += building[1].get_cps()
-                                    building[1].buy_one()
+                            if building.get_box().collidepoint(event.pos):
+                                if COOKIES >= building.get_price():
+                                    COOKIES -= building.get_price()
+                                    CPS += building.get_cps()
+                                    building.buy_one()
                     
 
 
@@ -119,20 +182,20 @@ def main():
         pg.draw.rect(screen, (100, 200, 70), cookie)
         pg.draw.rect(screen, (0, 0, 255), buildbox)
         TOTAL_FONT.render_to(screen, (20, 15), ("{:.0f}".format(COOKIES) + " Cookies"), (255,255,255))
-        CPS_FONT.render_to(screen, (20, 40), (str(CPS) + " Cookies Per Second"), (255,255,255))
+        CPS_FONT.render_to(screen, (20, 40), (str("{:.1f}".format(CPS)) + " Cookies Per Second"), (255,255,255))
 
-        buildings = [[curs_box, cursors], [grand_box, grandmas], [farm_box, farms]]
         y_shift = 10
         for building in buildings:
-            pg.draw.rect(screen, (255, 165, 0), building[0])
-            BLD_FONT.render_to(screen, (890, y_shift), (str(building[1].get_number())), (255,255,255))
-            BLD_FONT.render_to(screen, (990, y_shift), ("{:.2f}".format(building[1].get_price())), (255,255,255))
+            pg.draw.rect(screen, (255, 165, 0), building.get_box())
+            BLD_FONT.render_to(screen, (770, y_shift), (str(building.get_number())), (255,255,255))
+            BLD_FONT.render_to(screen, (790, y_shift), (str(building.get_name())), (255,255,255))
+            BLD_FONT.render_to(screen, (900, y_shift), ((str(m.ceil(building.get_price())))), (95,0,160))
             y_shift += 45
 
 
 
         pg.display.flip()
-        clock.tick(60)
+        clock.tick(30)
 
 
 if __name__ == '__main__':
